517ec203168339648f31515168bfe566fa0f336e
Lucas Monfort <Lucas_Monfort@etu.ube.fr>
2026-05-21 15:27:20 +0200

    ajout de plus de détail

--- frontend/src/components/ui/ItemDetailsCard.tsx
+++ frontend/src/components/ui/ItemDetailsCard.tsx
@@ -8,8 +8,17 @@ export default function ItemCard({ item }: ItemDetailsCardProps) {
     return (
         <div className="card bg-base-100 shadow-md hover:shadow-xl transition-shadow">
             <div className="card-body">
-                <h2 className="card-title">{item.name}</h2>
+                <h2 className="card-title">
+                    {item.name}
+                    <div className="badge badge-neutral">{item.id}</div>
+                </h2>
                 <p>{item.description}</p>
+
+                <div className="card-actions justify-end">
+                    <div className={`badge ${item.isResource ? 'badge-primary' : 'badge-secondary'}`}>{item.isResource ? <p>Resource</p> : <p>Craftable</p>}</div>
+                    <div className="badge badge-accent"> {item.sinkPoints} sink points </div>
+                    <div className="badge badge-neutral"> {item.energyValue} energy value </div>
+                </div>
             </div>
         </div>
     )
